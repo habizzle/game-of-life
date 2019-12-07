1bfdfed7189fe4a3d5af069fa709dfc6a25234a7
hannesb <hb.hannes.becker@gmail.com>
2019-12-07 10:59:52 +0100

    Adjust shell script for build and running app

--- run-terminal.sh
+++ run-terminal.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 
-./gradlew :presentation-terminal:clean :presentation-terminal:distTar
-tar -C "presentation-terminal/build/distributions/" -xf "presentation-terminal/build/distributions/gameoflife-terminal.tar"
-./presentation-terminal/build/distributions/gameoflife-terminal/bin/gameoflife-terminal
+./gradlew :presentation-terminal:installDist
+./presentation-terminal/build/install/gameoflife-terminal/bin/gameoflife-terminal
