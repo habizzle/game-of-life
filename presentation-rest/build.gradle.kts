b22b4d7c983a2c73cd7c6cf8ed9677378b763007
hannesb <hb.hannes.becker@gmail.com>
2020-03-15 13:52:41 +0100

    Logging for rest service

--- presentation-rest/build.gradle.kts
+++ presentation-rest/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 application {
-    mainClassName = "de.istkorrekt.gameoflife.presentation.rest.RestApplication"
+    mainClassName = "de.istkorrekt.gameoflife.presentation.rest.RestApplicationKt"
     applicationName = "gameoflife-rest"
 }
 
@@ -14,8 +14,10 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.sparkjava:spark-core:2.9.1")
     implementation("com.google.code.gson:gson:2.8.6")
-
+    implementation("org.slf4j:slf4j-api:1.7.30")
     implementation(rootProject)
+
+    runtimeOnly("ch.qos.logback:logback-classic:1.2.3")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.2")
