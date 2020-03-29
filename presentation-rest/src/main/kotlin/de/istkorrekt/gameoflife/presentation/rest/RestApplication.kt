de36d1217bf34d11645edb9d45043d73d4eedde4
hannesb <hb.hannes.becker@gmail.com>
2020-03-29 13:20:01 +0200

    Make compatible with CORS

--- presentation-rest/src/main/kotlin/de/istkorrekt/gameoflife/presentation/rest/RestApplication.kt
+++ presentation-rest/src/main/kotlin/de/istkorrekt/gameoflife/presentation/rest/RestApplication.kt
@@ -1,7 +1,27 @@
 package de.istkorrekt.gameoflife.presentation.rest
 
-import spark.Spark.put
+import spark.Filter
+import spark.Spark.*
 
 fun main() {
+    allowAllOrigins()
     put("/nextRound", "application/json", ::nextRound)
+}
+
+fun allowAllOrigins() {
+    options("/*") { request, response ->
+        val accessControlRequestHeaders: String? = request.headers("Access-Control-Request-Headers")
+        if (accessControlRequestHeaders != null) {
+            response.header("Access-Control-Allow-Headers", accessControlRequestHeaders)
+        }
+        val accessControlRequestMethod: String? = request.headers("Access-Control-Request-Method")
+        if (accessControlRequestMethod != null) {
+            response.header("Access-Control-Allow-Methods", accessControlRequestMethod)
+        }
+        "OK"
+    }
+
+    before(Filter { _, response ->
+        response.header("Access-Control-Allow-Origin", "*")
+    })
 }
